738c91b5da598e585801858bb4484bd531893e3f
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2022-03-09 11:38:14 -0600

    CommitInicial

---
+++ CinepolisApp/app/src/main/java/com/example/cinepolisapp/Vista/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.cinepolisapp.Vista;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cinepolisapp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    int cont;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("It works");
+        Log.d("Alert:","It works");
+
+        cont = 0;
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont++;
+                String text= "Boton presionado "+String.valueOf(cont)+" veces";
+                Toast.makeText(context, text, duration).show();
+
+
+            }
+        });
+    }
+
+}
