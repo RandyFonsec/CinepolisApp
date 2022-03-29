b89724c86667c8e442a2d430cf26f2ffa66ac3d0
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2022-03-28 23:13:31 -0600

    Botones inferiores funcionando

---
+++ CinepolisApp/app/src/main/java/com/example/cinepolisapp/Utils/Auxiliares.java
@@ -0,0 +1,59 @@
+package com.example.cinepolisapp.Utils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cinepolisapp.R;
+import com.example.cinepolisapp.Vista.AlimentoAdminActivity;
+import com.example.cinepolisapp.Vista.BillboardAdminActivity;
+import com.example.cinepolisapp.Vista.HomeAdminActivity;
+import com.example.cinepolisapp.Vista.UsersAdminActivity;
+
+public class Auxiliares {
+    public static void asignarBotonesInferiores(AppCompatActivity appCompat){
+        appCompat.findViewById(R.id.button12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(appCompat, UsersAdminActivity.class);
+                intent.putExtras(bundle);
+                appCompat.startActivity(intent);
+                appCompat.finish();
+            }
+        });
+        appCompat.findViewById(R.id.button13).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(appCompat, AlimentoAdminActivity.class);
+                intent.putExtras(bundle);
+                appCompat.startActivity(intent);
+                appCompat.finish();
+            }
+        });
+        appCompat.findViewById(R.id.button14).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(appCompat, BillboardAdminActivity.class);
+                intent.putExtras(bundle);
+                appCompat.startActivity(intent);
+                appCompat.finish();
+            }
+        });
+        appCompat.findViewById(R.id.button15).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Intent intent = new Intent(appCompat, HomeAdminActivity.class);
+                intent.putExtras(bundle);
+                appCompat.startActivity(intent);
+                appCompat.finish();
+            }
+        });
+
+    }
+}
