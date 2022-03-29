a70582ba9b72db9c52a9a72cb4bbf1dc710e7a7e
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2022-03-28 22:26:43 -0600

    Refactor interfaz Cambiado a botones

--- CinepolisApp/app/src/main/java/com/example/cinepolisapp/Vista/MainActivity.java
+++ CinepolisApp/app/src/main/java/com/example/cinepolisapp/Vista/MainActivity.java
@@ -37,80 +37,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_gestor_alimento);
-        Alerta.showAlert(MainActivity.this, "Hola","Esto es una alerta");
+        setContentView(R.layout.peliculas_disponibles);
         TabLayout t ;
-        t.
-/*
-        Spinner spinner = findViewById(R.id.EsquemaVacunacionSpinner);
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(0);
-        arrayList.add(1);
-        arrayList.add(2);
-        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this,
-                android.R.layout.simple_spinner_item, arrayList);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
 
-        System.out.println("It works");
-        Log.d("Alert:","It works");
-
-        cont = 0;
-
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
-
-
-        //probarAlimentos();*/
-
-
-    }
-
-    public void probarAlimentos() {
-        //Alimento alimento = new Alimento();
-        //alimento.setEliminado(0);
-        //alimento.setIdAlimento(1);
-        //alimento.setNombre("Agua");
-        //alimento.setCantidadDisponible(100);
-        //alimento.setPrecio(850.55);
-       // alimento.setIdTipoAlimento(2);
-        new AsyncTask<String, String, Alimento>() {
-            @Override
-            protected Alimento doInBackground(String... strings) {
-                AlimentoDAOImplementation i = AlimentoDAOImplementation.getInstance();
-                //return (Pelicula)i.create(p);
-                //Alimento al = (Alimento)i.create(alimento);
-                //Alimento al = (Alimento)i.get(1);
-                //alimento.setIdTipoAlimento(al.getIdTipoAlimento());
-                //alimento.setEliminado(al.isEliminado());
-                //Alimento al = (Alimento)i.create(alimento);
-                Alimento al = (Alimento)i.delete(1);
-                System.out.println(al.toString());
-                return al;
-                //Pelicula peli = (Pelicula)i.get(2);
-                //ArrayList<Pelicula> peliculas = (ArrayList<Pelicula>)i.getAll();
-                //for (Pelicula pelicula : peliculas)
-                //    System.out.println(pelicula.toString());
-                //return peliculas;
-
-
-            }
-            @Override
-            protected void onPostExecute(Alimento result) {
-                if(result != null) {
-                    //idT.setText(result.getId());
-                    //eM.setText(result.getEmail());
-                    //System.out.println(result.getTitulo());
-                    System.out.println("POSTEXECUTE");
-                }
-                else{
-                }
-
-            }
-            @Override
-            protected void onPreExecute(){
-                Toast.makeText(getApplicationContext(),"Iniciando consulta",Toast.LENGTH_SHORT).show();
-            }
-        }.execute();
-    }
         }
+}
