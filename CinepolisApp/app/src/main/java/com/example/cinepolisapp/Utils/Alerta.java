ad73cc84577b45041aa4abb7f298e4d749323a1e
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2022-03-28 20:24:35 -0600

    Alerta agregada

--- CinepolisApp/app/src/main/java/com/example/cinepolisapp/Utils/Alerta.java
+++ CinepolisApp/app/src/main/java/com/example/cinepolisapp/Utils/Alerta.java
@@ -1,5 +1,13 @@
 package com.example.cinepolisapp.Utils;
 
-public class Alerta {
+import android.app.AlertDialog;
+import android.content.Context;
 
+public class Alerta {
+    public static void showAlert(Context contexto, String titulo, String mensaje){
+        AlertDialog.Builder builder = new AlertDialog.Builder(contexto);
+        builder.setTitle(titulo).setMessage(mensaje);
+        AlertDialog alerta = builder.create();
+        alerta.show();
+    }
 }
